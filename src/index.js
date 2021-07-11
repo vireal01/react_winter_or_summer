d64dcfd5c6a9141757a43f73a2282dfe2e1be7d7
vireal01 <vireal014@yandex.ru>
2021-07-11 14:20:39 +0300

    add clock elemet

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay';
 import Loader from "./Loader"
+import './index.css'
 
 
 class App extends React.Component {
-    state = { lat: null, errorMessage: null }
+    state = { lat: null, errorMessage: null, time: new Date().toLocaleString() }
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude }),
             err => (this.setState({ errorMessage: err.message }))
         );
+        setInterval(() => this.setState({ time: new Date().toLocaleString() }), 1000)
+
     }
 
 
     renderContent() {
         if (!this.state.errorMessage && this.state.lat) {
-            return <div> <SeasonDisplay lat={this.state.lat} /></div>
+            return <div>
+                <SeasonDisplay lat={this.state.lat} />
+                <h3 className="clock">{this.state.time}</h3>
+            </div>
         }
         if (!this.state.lat && this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>
@@ -30,7 +36,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                {this.renderContent()}
+                <div>{this.renderContent()}</div>
+
             </div>
         )
     }
