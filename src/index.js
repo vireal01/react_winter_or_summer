8fd4944959e70701d7a1755b5ffc75a71eb70752
vireal01 <vireal014@yandex.ru>
2021-07-11 13:21:10 +0300

    summer or winter??

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay';
+import Loader from "./Loader"
+
+
+class App extends React.Component {
+    state = { lat: null, errorMessage: null }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => (this.setState({ errorMessage: err.message }))
+        );
+    }
+
+
+    renderContent() {
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div> <SeasonDisplay lat={this.state.lat} /></div>
+        }
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.lat && !this.state.errorMessage) {
+            return <Loader message='Please allow us to see your location' />
+        }
+    }
+    //React says we have to define render!
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    document.querySelector('#root')
+)
